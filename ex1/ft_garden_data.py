3068ea1a62ac0ab6e6628d529c8449c6267fee4e
spacotto <spacotto@student.42lehavre.fr>
2026-02-01 19:52:03 +0100

    Update ex1

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -1,9 +1,38 @@
 #!/usr/bin/env python3
 
 """
+This program track multiple plants with their information.
 """
 
 
-def 
+def ft_garden_data(name: str, height: int, age: int) -> None:
+    """
+    Text goes here
+    """
+    print(f" {name:<13}{f'{height}cm':<13}{f'{age} days':<13}")
 
 if __name__ == "__main__":
+    
+    # ANSI Color codes
+    white = "\033[1;97m"
+    reset = "\033[0m"
+
+    # Columns titles
+    c1 = "Name"
+    c2 = "Height"
+    c3 = "Age"
+    
+    # Fixed start of program print
+    print(f"\n{white} 🌱 Garden Plant Registry 🌱{reset}\n")
+    print(f" {white}{c1:<13}{c2:<13}{c3:<13}{reset}")
+    print(" --------------------------------------------------------")
+
+    # Add data entries
+    ft_garden_data("Rose", 25, 30)
+    ft_garden_data("Sunflower", 80, 45)
+    ft_garden_data("Cactus", 15, 120)
+
+    # Fixed end of program print
+    print("\n --------------------------------------------------------")
+    print(" End of Program\n")
+
