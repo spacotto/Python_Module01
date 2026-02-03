c985a36ec56d62249932d886ad506d0b79884f8f
Silvia Pacotto <spacotto@c1r6p4.42lehavre.fr>
2026-02-03 15:36:15 +0100

    update ex3: improvements & flake8

--- ex3/ft_plant_factory.py
+++ ex3/ft_plant_factory.py
@@ -6,12 +6,11 @@ Implementing an efficient Plant Factory using class initialisation.
 
 class Plant:
     """
-    A class representing a plant with immediate initialisation.
+    A class representing a plant.
     """
     def __init__(self, name: str, height: int, age: int) -> None:
         """
         Initialises the plant with starting values.
-        Requirement: Each plant should be ready to use immediately.
         """
         self.name: str = name
         self.height: int = height
@@ -19,21 +18,44 @@ class Plant:
 
     def display_creation(self) -> None:
         """
-        Displays the plant's data in the 'Factory Output' format.
+        Displays the current status of the plant.
         """
-        # Formats the string to match the 'Created: Name (Xcm, Y days)' style
-        info: str = f"({self.height}cm, {self.age} days)"
-        print(f"Created: {self.name:<10} {info}")
+        h_str: str = f"{self.height}cm"
+        a_str: str = f"{self.age} days"
+        print(f" {self.name:<13}{h_str:<13}{a_str:<13}")
+
+
+def display_header() -> None:
+    """
+    Displays the factory output header.
+    """
+    # ANSI Colors
+    white = "\033[1;97m"
+    reset = "\033[0m"
+
+    # Columns titles
+    c1, c2, c3 = "Name", "Height", "Age"
+
+    # Prints register header
+    print(f"\n{white} 🌱 Plant Factory: List of Plants Created 🌱{reset}\n")
+    print(f" {white}{c1:<13}{c2:<13}{c3:<13}{reset}")
+    print(" --------------------------------------------------------")
+
+
+def display_total(total: int) -> None:
+    """
+    Displays the total amount of created plants
+    """
+    # ANSI Colors
+    white = "\033[1;97m"
+    reset = "\033[0m"
+
+    print(f"\n{white} 🌱 Total plants created{reset}: {total}\n")
 
 
 if __name__ == "__main__":
-    """
-    Main function to simulate the Plant Factory.
-    """
-    print("=== Plant Factory Output ===")
-    
-    # Requirement: Create at least 5 different plants 
-    # We use a list to streamline the storage and display process.
+
+    # List of plants to create
     factory_batch: list[Plant] = [
         Plant("Rose", 25, 30),
         Plant("Oak", 200, 365),
@@ -42,8 +64,16 @@ if __name__ == "__main__":
         Plant("Fern", 15, 120)
     ]
 
-    # Iterate through the batch to display them
-    for plant in factory_batch:
-        plant.display_creation()
+    # Init total
+    total = 0
 
-    print(f"Total plants created: {len(factory_batch)}")
+    # Print header
+    display_header()
+
+    # Iterate through the batch to display them
+    for p in factory_batch:
+        p.display_creation()
+        total += 1
+
+    # Print total
+    display_total(total)
